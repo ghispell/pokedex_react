5c88f13fc657b23ffaf4158fde0371b64c1e8867
ghispell <ghis.pell@hotmail.fr>
2023-10-10 12:40:17 +0200

    React Basics 04

--- pokedex/src/components/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard.jsx
@@ -1,18 +1,9 @@
-const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "Mew",
 
-    },
-  ];
+function PokemonCard(props) {
+    console.log(props);
 
-function PokemonCard() {
-    const pokemon = pokemonList[0]
-
+    
+    const { pokemon } = props;
     return (
         <figure>
             <p>{pokemon.imgSrc === undefined ?"???" : <img src={pokemon.imgSrc} alt={pokemon.name}/>}</p>
